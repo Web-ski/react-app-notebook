5c57d24f52f3eb838a78780b59b156709b003938
Paweł Nieczuja-Ostrowski <pawel.nieczuja@onet.eu>
2020-12-10 22:59:49 +0100

    Init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        Eirmod ea dolor nonumy sit et lorem. Dolor lorem est nonumy kasd sit labore sit sea,
+        invidunt dolor sanctus sit.Learn React
+      </a>
+    </header>
+  </div>
+);
 export default App;
