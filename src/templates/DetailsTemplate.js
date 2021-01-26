499bd661bfe064db30874f34268fc658874463e7
Paweł Nieczuja-Ostrowski <pawel.nieczuja@onet.eu>
2021-01-26 20:44:21 +0100

    Update style for note

--- src/templates/DetailsTemplate.js
+++ src/templates/DetailsTemplate.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import UserPageTemplate from 'templates/UserPageTemplate';
 
 const DetailsTemplate = ({ children, pageType }) => (
-  <UserPageTemplate pageType={pageType}>
-    {children} <Link to="/">go back</Link>
-  </UserPageTemplate>
+  <UserPageTemplate pageType={pageType}>{children}</UserPageTemplate>
 );
 
 DetailsTemplate.propTypes = {
--- src/views/DetailsPage.js
+++ src/views/DetailsPage.js
@@ -1,8 +1,37 @@
 /* eslint-disable react/prop-types */
 import React, { useState, useEffect } from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import styled from 'styled-components';
+import { useRouteMatch, Link } from 'react-router-dom';
 import { routes } from 'routes';
 import DetailTemplate from 'templates/DetailsTemplate';
+import Heading from 'components/atoms/Heading/Heading';
+import Button from 'components/atoms/Button/Button';
+import Paragraph from 'components/atoms/Paragraph/Paragraph';
+
+const StyledWrapper = styled.div`
+  padding: 40px;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledWrapperNote = styled.div`
+  min-height: 50%;
+  padding: 40px;
+  box-shadow: 0 0px 30px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+`;
+
+const DateInfo = styled(Paragraph)`
+  margin: 0 0 5px;
+  font-weight: ${({ theme }) => theme.bold};
+  font-size: ${({ theme }) => theme.fontSize.s};
+`;
+
+const StyledHeading = styled(Heading)`
+  margin: 5px 0 0;
+`;
 
 const DetailsPage = () => {
   const [type, setType] = useState('notes');
@@ -27,7 +56,19 @@ const DetailsPage = () => {
 
   return (
     <DetailTemplate pageType={type}>
-      <p>{type}</p>
+      <StyledWrapper>
+        <StyledWrapperNote>
+          <StyledHeading>Super title about React</StyledHeading>
+          <DateInfo>date</DateInfo>
+          <Paragraph>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt labore molestias,
+            quis excepturi eveniet debitis iure eligendi enim fuga officia quasi culpa sit fugit at
+            recusandae quisquam nemo tenetur minima.
+          </Paragraph>
+          <Button activeColor={type}>Save/Close</Button>
+          <Link to="/">go back</Link>
+        </StyledWrapperNote>
+      </StyledWrapper>
     </DetailTemplate>
   );
 };
