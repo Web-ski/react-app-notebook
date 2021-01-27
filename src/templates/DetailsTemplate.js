366be8aa9214af1f4d2d2cb2af21c7156c4b7eb6
Paweł Nieczuja-Ostrowski <pawel.nieczuja@onet.eu>
2021-01-27 13:01:52 +0100

    Move code to DetailsTemplate

--- src/templates/DetailsTemplate.js
+++ src/templates/DetailsTemplate.js
@@ -1,18 +1,108 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import UserPageTemplate from 'templates/UserPageTemplate';
+import Heading from 'components/atoms/Heading/Heading';
+import Button from 'components/atoms/Button/Button';
+import Paragraph from 'components/atoms/Paragraph/Paragraph';
+import LINK_ICON from 'assets/link.svg';
 
-const DetailsTemplate = ({ children, pageType }) => (
-  <UserPageTemplate pageType={pageType}>{children}</UserPageTemplate>
+const StyledWrapper = styled.div`
+  padding: 40px;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledWrapperNote = styled.div`
+  position: relative;
+  padding: 40px;
+  box-shadow: 0 0px 30px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+`;
+
+const DateInfo = styled(Paragraph)`
+  margin: 0 0 5px;
+  font-weight: ${({ theme }) => theme.bold};
+  font-size: ${({ theme }) => theme.fontSize.s};
+`;
+
+const StyledHeading = styled(Heading)`
+  margin: 5px 0 0;
+`;
+
+const StyledImage = styled.img`
+  width: 85px;
+  height: 85px;
+  border: 5px solid ${({ theme }) => theme.grey2};
+  position: absolute;
+  border-radius: 50%;
+  right: 25px;
+  top: 20px;
+`;
+
+const StyledLink = styled.a`
+  display: block;
+  width: 47px;
+  height: 47px;
+  background: white url(${LINK_ICON});
+  background-size: 50%;
+  background-position: center;
+  background-repeat: no-repeat;
+  position: absolute;
+  border-radius: 50%;
+  right: 25px;
+  top: 20px;
+`;
+
+const DetailsTemplate = ({
+  pageType,
+  title,
+  created,
+  twitterName,
+  articleUrl,
+  content,
+  cardId,
+}) => (
+  <UserPageTemplate pageType={pageType}>
+    <StyledWrapper>
+      <StyledWrapperNote>
+        <StyledHeading big>{title}</StyledHeading>
+        <DateInfo>{created}</DateInfo>
+        <Paragraph>No {cardId}</Paragraph>
+        <Paragraph>{content}</Paragraph>
+        {pageType === 'twitters' && (
+          <StyledImage
+            src={`https://res.cloudinary.com/dthynrfew/image/twitter_name/${twitterName}.jpg`}
+          />
+        )}
+        {pageType === 'articles' && <StyledLink href={articleUrl} />}
+
+        <Button as={Link}
+to={`/${pageType}`}
+activeColor={pageType}>
+          Save/Close
+        </Button>
+      </StyledWrapperNote>
+    </StyledWrapper>
+  </UserPageTemplate>
 );
 
 DetailsTemplate.propTypes = {
-  children: PropTypes.element,
   pageType: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  created: PropTypes.string.isRequired,
+  twitterName: PropTypes.string,
+  articleUrl: PropTypes.string,
+  content: PropTypes.string.isRequired,
+  cardId: PropTypes.string.isRequired,
 };
 
 DetailsTemplate.defaultProps = {
-  children: null,
+  twitterName: null,
+  articleUrl: null,
 };
 
 export default DetailsTemplate;
--- src/views/DetailsPage.js
+++ src/views/DetailsPage.js
@@ -1,37 +1,8 @@
 /* eslint-disable react/prop-types */
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import { useRouteMatch, Link } from 'react-router-dom';
+import { useRouteMatch } from 'react-router-dom';
 import { routes } from 'routes';
 import DetailTemplate from 'templates/DetailsTemplate';
-import Heading from 'components/atoms/Heading/Heading';
-import Button from 'components/atoms/Button/Button';
-import Paragraph from 'components/atoms/Paragraph/Paragraph';
-
-const StyledWrapper = styled.div`
-  padding: 40px;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const StyledWrapperNote = styled.div`
-  min-height: 50%;
-  padding: 40px;
-  box-shadow: 0 0px 30px rgba(0, 0, 0, 0.1);
-  border-radius: 10px;
-`;
-
-const DateInfo = styled(Paragraph)`
-  margin: 0 0 5px;
-  font-weight: ${({ theme }) => theme.bold};
-  font-size: ${({ theme }) => theme.fontSize.s};
-`;
-
-const StyledHeading = styled(Heading)`
-  margin: 5px 0 0;
-`;
 
 const DetailsPage = () => {
   const [type, setType] = useState('notes');
@@ -54,22 +25,27 @@ const DetailsPage = () => {
     }
   }, [match]);
 
+  const mockyArticle = {
+    id: 1,
+    title: 'Super title about React',
+    content:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt labore molestias, quis excepturi eveniet debitis iure eligendi enim fuga officia quasi culpa sit fugit at recusandae quisquam nemo tenetur minima.',
+    twitterName: 'PaweNO1',
+    articleUrl: 'https://nieczuja-portfolio.pl',
+    created: '1 day',
+  };
+
   return (
-    <DetailTemplate pageType={type}>
-      <StyledWrapper>
-        <StyledWrapperNote>
-          <StyledHeading>Super title about React</StyledHeading>
-          <DateInfo>date</DateInfo>
-          <Paragraph>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt labore molestias,
-            quis excepturi eveniet debitis iure eligendi enim fuga officia quasi culpa sit fugit at
-            recusandae quisquam nemo tenetur minima.
-          </Paragraph>
-          <Button activeColor={type}>Save/Close</Button>
-          <Link to="/">go back</Link>
-        </StyledWrapperNote>
-      </StyledWrapper>
-    </DetailTemplate>
+    <DetailTemplate
+      pageType={type}
+      title={mockyArticle.title}
+      cardId={mockyArticle.id}
+      content={mockyArticle.content}
+      articleUrl={mockyArticle.articleUrl}
+      twitterName={mockyArticle.twitterName}
+      created={mockyArticle.created}
+      key={mockyArticle.title}
+    />
   );
 };
 
