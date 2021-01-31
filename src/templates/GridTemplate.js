740d3c4069dd4e136f362436b501b8ce83edfe83
Paweł Nieczuja-Ostrowski <pawel.nieczuja@onet.eu>
2021-01-31 09:19:15 +0100

    Add media queries to gridTemplate

--- src/templates/GridTemplate.js
+++ src/templates/GridTemplate.js
@@ -15,7 +15,16 @@ const StyledWrapper = styled.div`
 const StyledGrid = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  grid-gap: 70px;
+  grid-gap: 80px;
+
+  @media (max-width: 1400px) {
+    grid-gap: 40px;
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (max-width: 1100px) {
+    grid-template-columns: 1fr;
+  }
 `;
 
 const StyledPageHeader = styled.div`
