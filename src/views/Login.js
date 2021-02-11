c3380831cb8893cb8188b01a71910b35fca24a19
Paweł Nieczuja-Ostrowski <pawel.nieczuja@onet.eu>
2021-02-11 18:13:48 +0100

    Add form elems

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,6 +1,33 @@
 import React from 'react';
+import axios from 'axios';
 import Button from 'components/atoms/Button/Button';
+import { Formik, Form, Field } from 'formik';
 
-const Login = () => <Button>Send</Button>;
+const Login = () => {
+  const getTest = () => {
+    axios.get('http://localhost:3000/api/login/?get=1').then((response) => {
+      // handle success
+      console.log(response);
+    });
+  };
+  return (
+    <div>
+      <Formik initialValues={{ username: '', password: '' }}
+onSubmit={() => console.log('hej')}>
+        {() => (
+          <Form>
+            <Field name="username"
+type="text" />
+            <Field name="password"
+type="password" />
+            <button type="submit">Sign in</button>
+          </Form>
+        )}
+      </Formik>
+      <Button onClick={() => getTest()}>Get</Button>
+      <Button>Send</Button>
+    </div>
+  );
+};
 
 export default Login;
